88632bbdf07b3f22b09cca04ca7219e3da273e93
rishabh246 <rishabh246@gmail.com>
2020-11-13 21:44:16 +0000

    Cleanup done

--- template_cpp/src/include/FIFOBroadcast.hpp
+++ template_cpp/src/include/FIFOBroadcast.hpp
@@ -60,7 +60,6 @@ void fifobc_delivery(FIFOBroadcast *fifobc) {
       pending;
   while (1) {
     AppMessage msg = URB_recv(&(fifobc->urb));
-    // std::cout << identifier << msg.stringify() << "\n" << std::flush;
     if (pending.find(msg.source) == pending.end()) {
       std::priority_queue<unsigned long, std::vector<unsigned long>,
                           std::greater<unsigned long>>
